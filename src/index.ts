fce1c158b5ac5be181d8de6209dc77b3f8625855
JunSeok <ppu1234@naver.com>
2022-06-12 23:58:31 +0900

    0612_commit

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,19 @@ const input = document.getElementById("input") as HTMLInputElement
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
+
+  if (input.value == "" || input.value == null) return
+
+  const goal = input.value
+
+  addGoalItem(goal)
 })
+
+function addGoalItem(goal: string) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  label.append(checkbox, goal)
+  item.append(label)
+}
