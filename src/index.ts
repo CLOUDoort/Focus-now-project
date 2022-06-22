5aa22fbf7098953f7fd49b0b38052a04c849b68d
JunSeok <ppu1234@naver.com>
2022-06-22 23:44:26 +0900

    0622_commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 const form = document.getElementById("form") as HTMLFormElement
 const input = document.getElementById("input") as HTMLInputElement
+const tough = document.getElementById("tough") as HTMLButtonElement
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
